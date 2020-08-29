be29105fbc705b5ceda6d240bfba14219bdeede3
Kade <kaderderk@gmail.com>
2020-08-29 18:42:05 -0500

    Adding Database project

---
+++ BookApp.Data/dbo/Tables/Books.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [Id]       INT             IDENTITY (1, 1) NOT NULL,
+    [AuthorID] INT             NOT NULL,
+    [Genre]    NVARCHAR (MAX)  NOT NULL,
+    [Price]    DECIMAL (18, 2) NOT NULL,
+    [Publiser] NVARCHAR (MAX)  NULL,
+    [Title]    NVARCHAR (MAX)  NOT NULL,
+    [Year]     INT             NOT NULL,
+    CONSTRAINT [PK_Books] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Books_Authors_AuthorID] FOREIGN KEY ([AuthorID]) REFERENCES [dbo].[Authors] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Books_AuthorID]
+    ON [dbo].[Books]([AuthorID] ASC);
+
---
+++ BookApp.Data/dbo/Tables/Reviews.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Reviews] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Body]     NVARCHAR (MAX) NULL,
+    [BookId]   INT            NOT NULL,
+    [Rating]   INT            NOT NULL,
+    [UserName] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Reviews] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Reviews_Books_BookId] FOREIGN KEY ([BookId]) REFERENCES [dbo].[Books] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Reviews_BookId]
+    ON [dbo].[Reviews]([BookId] ASC);
+
