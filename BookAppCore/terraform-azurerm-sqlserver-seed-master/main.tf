bd26512628ea66bd9df8fe006729468850bd7128
oja24 <43191931+oja24@users.noreply.github.com>
2021-06-22 18:27:56 -0400

    Update main.tf

--- BookAppCore/terraform-azurerm-sqlserver-seed-master/main.tf
+++ BookAppCore/terraform-azurerm-sqlserver-seed-master/main.tf
@@ -13,16 +13,16 @@
 
 
 resource azurerm_sql_database db {
-  name                             = ${var.db_name}
-  location                         = ${var.location}
-  resource_group_name              = ${var.resource_group}
-  server_name                      = ${local.db_server[0]}
-  edition                          = ${var.db_edition}
-  collation                        = ${var.collation}
-  requested_service_objective_name = ${var.service_objective_name}
+  name                             = var.db_name
+  location                         = var.location
+  resource_group_name              = var.resource_group
+  server_name                      = local.db_server[0]
+  edition                          = var.db_edition
+  collation                        = var.collation
+  requested_service_objective_name = var.service_objective_name
   create_mode                      = Default
 
   provisioner local-exec {
-    command = sqlcmd -U ${var.sql_admin_username} -P ${var.sql_admin_password} -S ${var.db_server_fqdn} -d ${var.db_name} -i ${var.init_script_file} -o ${var.log_file}
+    command = sqlcmd -U var.sql_admin_username -P var.sql_admin_password -S var.db_server_fqdn -d var.db_name -i var.init_script_file -o var.log_file
   }
 }
