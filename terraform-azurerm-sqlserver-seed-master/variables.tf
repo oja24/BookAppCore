2d6c47b690a5e6915fd263e35442d90576ef5807
oja24 <43191931+oja24@users.noreply.github.com>
2021-06-22 20:07:00 -0400

    Add files via upload

--- terraform-azurerm-sqlserver-seed-master/variables.tf
+++ terraform-azurerm-sqlserver-seed-master/variables.tf
@@ -1,60 +1,43 @@
+variable "presentation" {
+    description = "The name of the presentation - used for tagging Azure resources so I know what they belong to"
+    default = "__Presentation__"
+}
+
+variable "ResourceGroupName" {
+  description = "The Prefix used for all resources in this example"
+  default     = "__ResourceGroupName__"
+}
+
 variable "location" {
-  description = "Azure location where the db server is located."
+  description = "The Azure Region in which the resources in this example should exist"
+  default     = "__location__"
 }
 
-variable "resource_group" {
-  description = "Azure Resource Group name where the db server is located."
+variable "SqlServerName" {
+  description = "The name of the Azure SQL Server to be created or to have the database on - needs to be unique, lowercase between 3 and 24 characters including the prefix"
+  default     = "__SqlServerName__"
 }
 
-variable "db_server_fqdn" {
-  description = "Full qualified domain name of the database sever. The server where to create the database. In the form xxxx.database.windows.net"
+variable "SQLServerAdminUser" {
+  description = "The name of the Azure SQL Server Admin user for the Azure SQL Database"
+  default     = "__SQLServerAdminUser__"
+}
+variable "SQLServerAdminPassword" {
+  description = "The Azure SQL Database users password"
+  default     = "__SQLServerAdminPassword__"
+}
+variable "SqlDatabaseName" {
+  description = "The name of the Azure SQL database on - needs to be unique, lowercase between 3 and 24 characters including the prefix"
+  default     = "__SqlDatabaseName__"
 }
 
-variable "db_name" {
-  description = "The name of the database to be created."
+
+variable "Edition" {
+  description = "The Edition of the Database - Basic, Standard, Premium, or DataWarehouse"
+  default     = "__Edition__"
 }
 
-variable "sql_admin_username" {
-  description = "The administrator username of the SQL Server to use for launching the seed script."
+variable "ServiceObjective" {
+  description = "The Service Tier S0, S1, S2, S3, P1, P2, P4, P6, P11 and ElasticPool"
+  default     = "__ServiceObjective__"
 }
-
-variable "sql_admin_password" {
-  description = "The administrator password of the SQL Server to use for launching the seed script."
-}
-
-variable "tags" {
-  description = "The tags to associate with the database."
-  type        = "map"
-
-  default = {
-    provisioner = "terraform"
-  }
-}
-
-variable init_script_file {
-  description = "SQL Script file name to seed the database. Example: db-init.sql."
-}
-
-variable log_file {
-  description = "Log file name to create with the seeding results."
-  default     = "db-init.log"
-}
-
-variable "collation" {
-  description = "The collation for the database. Default is SQL_Latin1_General_CP1_CI_AS"
-  default     = "SQL_Latin1_General_CP1_CI_AS"
-}
-
-variable "db_edition" {
-  description = "The edition of the database to be created."
-  default     = "Basic"
-}
-
-variable "service_objective_name" {
-  description = "The performance level for the database. For the list of acceptable values, see https://docs.microsoft.com/en-gb/azure/sql-database/sql-database-service-tiers. Default is Basic."
-  default     = "Basic"
-}
-
-/* variable "azurerm_sql_firewall_rule" {
-  description = "firewall rule"
-} */
